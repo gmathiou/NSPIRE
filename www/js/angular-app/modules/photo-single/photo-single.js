4e5a0f4ddb5a5137f772386f8bcef52b2e70c93c
Georgios Mathioudakis <mathioudakis.giorgos@gmail.com>
2016-02-27 18:12:38 +0200

    Add google maps in photos

--- www/js/angular-app/modules/photo-single/photo-single.js
+++ www/js/angular-app/modules/photo-single/photo-single.js
@@ -1,18 +1,37 @@
 "use strict";
 
-var photoSingle = angular.module('photo.single', ['ionic', 'ngCordova', '500px.service'])
+var photoSingle = angular.module('photo.single', ['ionic', 'ngCordova', '500px.service', 'uiGmapgoogle-maps'])
 
-photoSingle.controller('PhotoSingleController', ['$rootScope', '$scope', 'FiveHundredService', '$cordovaGeolocation', '$stateParams', function ($rootScope, $scope, FiveHundredService, $cordovaGeolocation, $stateParams) {
+photoSingle.controller('PhotoSingleController', ['$rootScope', '$scope', 'FiveHundredService', '$cordovaGeolocation', '$stateParams', '$ionicPlatform', function ($rootScope, $scope, FiveHundredService, $cordovaGeolocation, $stateParams, $ionicPlatform) {
 
     $scope.photoId = $stateParams.photoid;
     $scope.photoItem = null;
-    
+
     if ($scope.photoId) {
         FiveHundredService.getPhoto($scope.photoId).then(function (data) {
             $scope.photoItem = data;
+            $scope.initMap();
         }, function (error) {
 
         });
-    }
+    };
 
+    $scope.initMap = function () {
+        $scope.map = {
+            center: {
+                latitude: $scope.photoItem.photo.latitude,
+                longitude: $scope.photoItem.photo.longitude
+            },
+            zoom: 16
+        };
+
+        $scope.marker = {
+            id: 0,
+            coords: {
+                latitude: $scope.photoItem.photo.latitude,
+                longitude: $scope.photoItem.photo.longitude
+            },
+            options: { draggable: true },
+        };
+    };
 }]);
